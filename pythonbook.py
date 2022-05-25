b72d423bd0fe56dfc67642c1ee78bc7eec391715
Smittys3 <ssmith@me.com>
2022-05-25 15:06:46 -0500

    added content

---
+++ pythonbook.py
@@ -0,0 +1,10 @@
+from ssl import ALERT_DESCRIPTION_BAD_CERTIFICATE
+
+
+famous = 'stephen'
+print(famous)
+
+name = "Ada Lovelace"
+print(name.upper())
+print(name.lower())
+
--- test.py
+++ test.py
@@ -1,3 +1,9 @@
 print('Hi there')
 
+
+
+
 print(3 + 5)
+print('Hello we made this in the CLI')
+
+
